bafe2117c32b672502642741099e652eb7ea2187
attachment4 <karavashkinivan1995@gmail.com>
2026-03-22 02:02:47 -0800

    Change new-file

---
+++ src/Main.java
@@ -0,0 +1,21 @@
+public class Main {
+    public static void main(String[] args) {
+
+        int refilBalance = 1000;
+        int balance = 100;
+
+
+        int bonusRubRefil;
+        if (refilBalance >= 1000) {
+            bonusRubRefil = refilBalance / 100;
+        }
+        else{
+            bonusRubRefil = 0;
+        }
+
+
+        int statusBalance = balance + refilBalance + bonusRubRefil;
+        System.out.println("Бонусные рубли:" + bonusRubRefil);
+        System.out.println("Итоговый счет:" + statusBalance);
+    }
+}
